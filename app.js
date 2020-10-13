a9ebbdc518363662bf7941b3b96df27b3b3027c9
goater <goater1095@gmail.com>
2020-10-13 20:37:26 +0800

    feat: project ver1 - initial

---
+++ app.js
@@ -0,0 +1,32 @@
+const { static } = require('express')
+const express = require('express')
+const app = express()
+const exhbs = require('express-handlebars')
+const resList = require('./restaurant.json')
+const port = 3000
+
+app.use(express.static('public'))
+
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index', { resList: resList.results })
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const findRes = resList.results.find(rest => rest.id.toString() === id)
+  console.log(id)
+  res.render('show', { rest: findRes })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
+  const searchRes = resList.results.filter(rest => rest.name.toLowerCase().includes(keyword))
+  res.render('index', { resList: searchRes })
+})
+
+app.listen(port, () => {
+  console.log(`This Server is start on http://localhost:${port}`)
+})
